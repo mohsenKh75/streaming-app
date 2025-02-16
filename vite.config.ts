ce2c445d41c662a441278fab9b3e2b2850d5d9a7
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-02-16 23:06:10 +0330

    feat: add configs - libs

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { '@': path.resolve(__dirname, './src') } }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      figma: path.resolve(__dirname, './figma')
+    }
+  }
 });
