7b7babff8dd3cd1a471eff7200d74db071fffc09
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-02-20 12:54:32 +0330

    fix: style issue in layout

--- src/components/Layout/MainLayoutContainer.tsx
+++ src/components/Layout/MainLayoutContainer.tsx
@@ -6,14 +6,19 @@ import { Footer } from './Footer';
 
 export function MainLayoutContainer({ children }: { children: React.ReactNode }) {
   return (
-    <GridContainer direction='flex-col' className='mx-auto min-h-screen max-w-[480px] h-full' dir='rtl'>
-      <Box tag='header'>
+    <GridContainer
+      backgroundColor='bg-neutral-pure-black'
+      direction='flex-col'
+      className='mx-auto min-h-screen max-w-[480px] h-full'
+      dir='rtl'
+    >
+      <Box tag='header' className='fixed w-full z-10'>
         <Header />
       </Box>
-      <Box tag='main' className='flex-1 container px-4'>
+      <Box tag='main' className='flex-1 container mt-[41px]'>
         {children}
       </Box>
-      <Box backgroundColor='bg-neutral-pure-black' tag='footer' className='p-4 text-center'>
+      <Box backgroundColor='bg-neutral-pure-black' tag='footer' className='p-4 text-center fixed bottom-0 z-10 w-full'>
         <Footer />
       </Box>
     </GridContainer>
