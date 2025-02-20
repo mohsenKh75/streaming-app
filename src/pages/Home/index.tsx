25a61087113ade3daa11ad8d560daae4234caf98
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-02-20 12:55:26 +0330

    feat: create home page with bg image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,40 @@
+import { Box } from '@/components/core/Box';
+import { GridContainer } from '@/components/core/GridContainer';
+import { Typography } from '@/components/core/Typography';
+import { HeaderContent } from '@/components/pages/Home/HeaderContent';
+import { headerContent } from '@/mock/headerMockData';
+import { useEffect, useState } from 'react';
+
+const backgroundHeight = 132;
 function Home() {
-  return <div className='bg-slate-500'>this is mainPage</div>;
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => {
+    setOffsetY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <>
+      <Box
+        style={{
+          transform: `translateY(${offsetY * 0.5}px)`,
+          height: `${backgroundHeight}px`,
+          backgroundImage: `url(/img/sex-life.jpg)`,
+          backgroundAttachment: 'fixed',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundColor: '#efefef',
+          boxShadow: 'inset 0 0 80px rgba(0, 0, 0, 0.7)'
+        }}
+      />
+      <Box className='px-2'>
+        <HeaderContent headerContent={headerContent} />
+      </Box>
+    </>
+  );
 }
 export default Home;
